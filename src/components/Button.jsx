071092485a95541e9237db95e1c9d0ba5d01a605
Kirill Lyashenko <tltelf@yandex.ru>
2023-12-17 11:52:13 +0400

    add: Special Offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,20 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+	label,
+	iconURL,
+	backgroundColor,
+	textColor,
+	borderColor,
+}) => {
 	return (
-		<button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+		<button
+			className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+			${
+				backgroundColor
+					? `${backgroundColor} ${textColor} ${borderColor}`
+					: 'text-white border-coral-red bg-coral-red'
+			}
+		rounded-full`}
+		>
 			{label}{' '}
 			{iconURL && (
 				<img
